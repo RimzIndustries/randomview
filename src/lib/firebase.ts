817efc351db11d998efa4d3fa986a3ec9a807e35
Rimz industries <rimzindustries@gmail.com>
2025-09-24 01:53:55 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCC_eSsGc4l4x7nllKCzvv0t5ofE0UhKLI",
+  authDomain: "randomview-ce4b6.firebaseapp.com",
+  projectId: "randomview-ce4b6",
+  storageBucket: "randomview-ce4b6.firebasestorage.app",
+  messagingSenderId: "240811585287",
+  appId: "1:240811585287:web:0e989a03931ae74808dfa2",
+  measurementId: "G-WZFYC0FZC7"
 };
 
 // Initialize Firebase
